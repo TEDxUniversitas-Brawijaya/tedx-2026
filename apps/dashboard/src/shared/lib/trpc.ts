0ea7e98d963c2382777c00f619d0832a084cdaf8
ahargunyllib <billy.bpm03@gmail.com>
2026-04-07 13:43:40 +0700

    feat(trpc): refactor TRPC client setup to use splitLink for conditional handling

--- apps/dashboard/src/shared/lib/trpc.ts
+++ apps/dashboard/src/shared/lib/trpc.ts
@@ -1,20 +1,41 @@
 import type { TRPCRouter } from "@tedx-2026/api";
-import { createTRPCClient, httpBatchStreamLink } from "@trpc/client";
+import {
+  createTRPCClient,
+  httpBatchStreamLink,
+  httpLink,
+  isNonJsonSerializable,
+  splitLink,
+} from "@trpc/client";
 import { createTRPCOptionsProxy } from "@trpc/tanstack-react-query";
 import SuperJSON from "superjson";
 import { queryClient } from "./query-client";
 
 export const trpcClient = createTRPCClient<TRPCRouter>({
   links: [
-    httpBatchStreamLink({
-      url: `${import.meta.env.VITE_PUBLIC_API_URL || "http://localhost:3000"}/trpc`,
-      transformer: SuperJSON,
-      fetch(_url, options) {
-        return fetch(_url, {
-          ...options,
-          credentials: "include",
-        });
+    splitLink({
+      condition(op) {
+        return isNonJsonSerializable(op.input);
       },
+      true: httpLink({
+        url: `${import.meta.env.VITE_PUBLIC_API_URL || "http://localhost:3000"}/trpc`,
+        transformer: SuperJSON,
+        fetch(url, options) {
+          return fetch(url, {
+            ...options,
+            credentials: "include",
+          });
+        },
+      }),
+      false: httpBatchStreamLink({
+        url: `${import.meta.env.VITE_PUBLIC_API_URL || "http://localhost:3000"}/trpc`,
+        transformer: SuperJSON,
+        fetch(url, options) {
+          return fetch(url, {
+            ...options,
+            credentials: "include",
+          });
+        },
+      }),
     }),
   ],
 });
