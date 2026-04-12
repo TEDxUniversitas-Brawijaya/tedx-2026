30795542ea9ac7e6f192a75ef9a3f7148ca80e5a
Xfhreall <farelrizqi377@gmail.com>
2026-04-12 19:02:14 +0700

    feat: reorganize exports for queries in the database index

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,3 +1,10 @@
 export { createDB, type D1Database, type DB } from "./db";
-export { createUserQueries, type UserQueries } from "./queries";
+export {
+  createConfigQueries,
+  createMerchQueries,
+  createUserQueries,
+  type ConfigQueries,
+  type MerchQueries,
+  type UserQueries,
+} from "./queries";
 export { schema } from "./schemas";
