31913e5bc9f2ffd7a8b9fe41aed04600d479be45
Xfhreall <farelrizqi377@gmail.com>
2026-04-16 20:42:11 +0700

    feat: add export for createRefundQueries and type RefundQueries in index

--- packages/db/src/queries/index.ts
+++ packages/db/src/queries/index.ts
@@ -1,4 +1,5 @@
 export { createConfigQueries, type ConfigQueries } from "./config";
 export { createOrderQueries, type OrderQueries } from "./order";
 export { createProductQueries, type ProductQueries } from "./product";
+export { createRefundQueries, type RefundQueries } from "./refund";
 export { createUserQueries, type UserQueries } from "./user";
